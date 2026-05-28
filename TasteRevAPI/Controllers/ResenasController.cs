e030122a9096b0a7ae84956b37c1a82e31f0ab35
Raul <l22330493@hermosillo.tecnm.mx>
2026-05-27 23:37:53 -0700

    Creación de ResenasController y finalización del backend API

---
+++ TasteRevAPI/Controllers/ResenasController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TasteRevAPI.Data;
+using TasteRevAPI.Models;
+
+namespace TasteRevAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ResenasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ResenasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Resenas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Resena>>> GetResenas()
+        {
+            return await _context.Resenas.ToListAsync();
+        }
+
+        // GET: api/Resenas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Resena>> GetResena(int id)
+        {
+            var resena = await _context.Resenas.FindAsync(id);
+            if (resena == null) return NotFound();
+
+            return resena;
+        }
+
+        // POST: api/Resenas
+        [HttpPost]
+        public async Task<ActionResult<Resena>> PostResena(Resena resena)
+        {
+            _context.Resenas.Add(resena);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetResena), new { id = resena.Id }, resena);
+        }
+
+        // PUT: api/Resenas/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutResena(int id, Resena resena)
+        {
+            if (id != resena.Id) return BadRequest();
+
+            _context.Entry(resena).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ResenaExists(id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Resenas/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteResena(int id)
+        {
+            var resena = await _context.Resenas.FindAsync(id);
+            if (resena == null) return NotFound();
+
+            _context.Resenas.Remove(resena);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ResenaExists(int id)
+        {
+            return _context.Resenas.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ TasteRevAPI/Controllers/SeriesController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TasteRevAPI.Data;
+using TasteRevAPI.Models;
+
+namespace TasteRevAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SeriesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SeriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Series
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Serie>>> GetSeries()
+        {
+            return await _context.Series.ToListAsync();
+        }
+
+        // GET: api/Series/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Serie>> GetSerie(int id)
+        {
+            var serie = await _context.Series.FindAsync(id);
+            if (serie == null) return NotFound();
+
+            return serie;
+        }
+
+        // POST: api/Series
+        [HttpPost]
+        public async Task<ActionResult<Serie>> PostSerie(Serie serie)
+        {
+            _context.Series.Add(serie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetSerie), new { id = serie.Id }, serie);
+        }
+
+        // PUT: api/Series/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSerie(int id, Serie serie)
+        {
+            if (id != serie.Id) return BadRequest();
+
+            _context.Entry(serie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SerieExists(id)) return NotFound();
+                else throw;
+            }
+
+            return NoContent();
+        }
+
+        // DELETE: api/Series/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSerie(int id)
+        {
+            var serie = await _context.Series.FindAsync(id);
+            if (serie == null) return NotFound();
+
+            _context.Series.Remove(serie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SerieExists(int id)
+        {
+            return _context.Series.Any(e => e.Id == id);
+        }
+    }
+}
